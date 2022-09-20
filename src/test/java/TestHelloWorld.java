4cebdeafb34551b9d2783eefa0ed28637678bd64
Uka Enkbold <uka.enkbold@gmail.com>
2022-09-20 10:55:50 -0500

    "test passed"

--- src/test/java/TestHelloWorld.java
+++ src/test/java/TestHelloWorld.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertEquals;
  * Created by LaunchCode
  */
 public class TestHelloWorld {
-
     @Test
     public void testSayHello() {
         assertEquals("Hello, World!", HelloWorld.sayHello());
